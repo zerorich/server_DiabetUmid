a7eaa69bdc364589d5c38381df04e26e00be6eb7
zerorich <167667718+zerorich@users.noreply.github.com>
2025-03-21 20:25:25 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
+require("dotenv").config(); // Подключаем dotenv
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors"); // ✅ Исправлено
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-app.use(express.json()); 
-app.use(cors()); // ✅ Разрешаем CORS для всех доменов
+app.use(express.json());
+app.use(cors());
 
 app.use("/users", userRoutes);
 
-// Подключение к MongoDB
-mongoose.connect("mongodb+srv://zerorich207:zerorich207aliasi@cluster0.julfn.mongodb.net/", {
+// Подключение к MongoDB через переменную окружения
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("✅ Connected to MongoDB"))
   .catch(err => console.error("❌ Could not connect to MongoDB:", err));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
